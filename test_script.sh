5860b13af10cc3156f463b682b26ee1303dfdda2
Emre Demirors <emre.demirors@pason.com>
2015-05-20 11:18:16 -0500

    updated test script

--- test_script.sh
+++ test_script.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 # Add your tests here
 
-wmls -r $WITSML_STORE_URL -t 60 -u $WITSML_USER -p $WITSML_PASS -t $WITSML_TIMEOUT -q /root/wmls/query_v14/get_all_wells.xml -a get
+wmls -r $WITSML_STORE_URL -u $WITSML_USER -p $WITSML_PASS -t $WITSML_TIMEOUT -q /root/wmls/query_v14/get_all_wells.xml -a get
